789c756b973d89901d357c1aba70a816cb6759a7
cartland <c.cartland@gmail.com>
2017-09-06 22:38:14 -0700

    A

---
+++ A/a/src/main/java/a/a/A.java
@@ -0,0 +1,13 @@
+package a.a;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class A extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a);
+    }
+}
